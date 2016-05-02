3086cb19785ce53a2bc60128704952ce5ebe8b3f
Kai Hendry <hendry@webconverger.com>
2016-05-02 14:49:17 +0800

    Snapshot

--- check.sh
+++ check.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 find mediadir -type f -not \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.mp4" -o -iname "*.mov" \) -o -path '*/\.*'
---
+++ delete-empty-dirs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+find mediadir -type d -empty -delete
