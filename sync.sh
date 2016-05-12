9f154eb06618528b8bd88fceb7f1b8c8a5b34c3d
Kai Hendry <hendry@webconverger.com>
2016-05-12 10:37:31 +0800

    Tweaks

--- sync.sh
+++ sync.sh
@@ -2,6 +2,7 @@
 ddir=/mnt/raid1/kai
 if ! test -d $ddir
 then
-	mount /mnt/raid1 \# first
+	echo Please: mount /mnt/raid1
+	exit
 fi
-rsync --remove-source-files -Pr out/ $ddir
+rsync --remove-source-files -Pr "${1:-$HOME/media/out/}" $ddir
