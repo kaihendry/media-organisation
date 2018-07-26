c3b0cb91fbcd29c8e2c904af6b0a82767c647845
Kai Hendry <hendry@webconverger.com>
2018-07-26 12:32:10 +0800

    shellchecked

--- v2/1_build-date-tree.sh
+++ v2/1_build-date-tree.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
+shopt -s nocaseglob
 
 moving=$(mktemp)
 
 # we require a directory
 test -d "$1" || exit
 
-dir=$1
+dir="$1"
 
-find $dir -maxdepth 1 | while read -r media
+find "$dir" -maxdepth 1 | while read -r media
 do
 	case $media in
-		*.jpg|*.mp4|*.JPG|*.MP4|*.fcpbundle)
-			dateprefix="mysrctree/$(stat -c %y "$media" | awk '{print $1}')/$(basename $media)"
-			mkdir -p $(dirname $dateprefix)
-			echo $media $dateprefix >> $moving
+		*.jpg|*.mp4|*.fcpbundle)
+			dateprefix="mysrctree/$(stat -c %y "$media" | awk '{print $1}')/$(basename "$media")"
+			mkdir -p "$(dirname "$dateprefix")"
+			echo "$media" "$dateprefix" >> "$moving"
 			;;
 		*)
-		echo Ignoring $media
+		echo Ignoring "$media"
 	esac
 done
 
 # Nothing to move, we exit
-test -s $moving || exit
+test -s "$moving" || exit
 
-cat $moving
+cat "$moving"
 
 read -p "Are you sure? " -n 1 -r
 echo
@@ -32,5 +33,5 @@ then
     while read -r src dest
 	do
 		mv -v "$src" "$dest"
-	done < $moving
+	done < "$moving"
 fi
