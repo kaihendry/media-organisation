10b15c86616df47b9638ebdccb74391980cef2e0
Kai Hendry <hendry@webconverger.com>
2018-10-30 12:08:01 +0800

    No transcoded media either

--- v2/2_sync-mysrctree.sh
+++ v2/2_sync-mysrctree.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync --remove-source-files -vaihhHSP --exclude "*Render Files*" --exclude "*Analysis Files*" --omit-dir-times mysrctree/ freenas:/mnt/red/redsamba
+rsync --remove-source-files -vaihhHSP --exclude "Transcoded Media" --exclude "*Render Files*" --exclude "*Analysis Files*" --omit-dir-times mysrctree/ freenas:/mnt/red/redsamba
