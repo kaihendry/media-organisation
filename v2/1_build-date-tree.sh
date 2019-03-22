8e88e70d9e03c72f027aebbac9aae79a1a865fda
Kai Hendry <hendry@webconverger.com>
2019-03-22 10:49:53 +0800

    Refactor to allow selection

--- v2/1_build-date-tree.sh
+++ v2/1_build-date-tree.sh
@@ -10,13 +10,57 @@ esac
 
 moving=$(mktemp) || exit
 
-for i in "$@"
-do
+# Usage info
+show_help() {
+cat << EOF
+Usage: ${0##*/} [-hvs] [DIR]
+Stage files for a backup
 
+    -h          display this help and exit
+    -s          select files
+    -v          verbose mode. Can be used multiple times for increased
+                verbosity.
+EOF
+}
+
+# Initialize our own variables:
+verbose=0
+select=0
+
+OPTIND=1
+
+while getopts hvs opt; do
+    case $opt in
+        h)
+            show_help
+            exit 0
+            ;;
+        v)  verbose=$((verbose+1))
+            ;;
+        s)  select=$((select+1))
+            ;;
+        *)
+            show_help >&2
+            exit 1
+            ;;
+    esac
+done
+shift "$((OPTIND-1))"   # Discard the options and sentinel --
+
+dirs=$@
+
+if ! test "$dirs"
+then
+	echo No args
+	dirs=~/Movies
+fi
+
+for i in $dirs
+do
 # we require a directory
 if ! test -d "$i"
 then
-	echo ERROR: Arg1 \""$i"\" must be a directory 1>&2
+	echo ERROR: \""$i"\" must be a directory 1>&2
 	exit 1
 fi
 
@@ -39,6 +83,16 @@ done
 
 # Nothing to move, we exit
 test -s "$moving" || exit
+
+if test "$select" -eq 1
+then
+	select dir in $(awk '{print $1}' $moving)
+	do
+		grep $dir $moving > $moving.bak
+		mv $moving.bak $moving
+		break
+	done
+fi
 
 cat "$moving"
 
