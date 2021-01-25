e84a24142cc34de58639fd68a8d78a8b48e5804d
Kai Ian Hendry <hendry+mbp@iki.fi>
2021-01-25 16:27:27 +0800

    Better

--- v2/2_sync-mysrctree.sh
+++ v2/2_sync-mysrctree.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -vaihhHSP --exclude "Transcoded Media" --exclude=".*" --exclude "*Render Files*" --exclude "*Analysis Files*" --omit-dir-times mysrctree/ 192.168.1.2:/mnt/red/redsamba
+rsync -LvrihhHSP --remove-source-files --exclude "Transcoded Media" --exclude=".*" --exclude "*Render Files*" --exclude "*Analysis Files*" --omit-dir-times mysrctree/ 192.168.1.2:/mnt/red/redsamba
